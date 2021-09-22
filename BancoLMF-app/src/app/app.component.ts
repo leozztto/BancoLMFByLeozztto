4cc0f7dcbb3c0691fde1d9d77fae83241591d35b
leandro <leozztto@gmail.com>
2021-09-21 23:54:54 -0300

    Adicionado Template em angular material

--- BancoLMF-app/src/app/app.component.ts
+++ BancoLMF-app/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BancoLMF-app';
+  sideBarOpen = true;
+
+  sideBarToggler() {
+    this.sideBarOpen = !this.sideBarOpen;
+  }
 }
